b411bb45f1f642339389b3c20bd24aa72a5bc6de
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2023-05-14 15:30:03 +0900

    属性で設定

--- resources/views/components/tests/app.blade.php
+++ resources/views/components/tests/app.blade.php
@@ -16,6 +16,9 @@
         <script src="{{ asset('js/app.js')}}" defer></script>
     </head>
     <body>
+		<head>
+			{{ $header }}
+		</head>
         <div class="font-sans text-gray-900 antialiased">
             {{ $slot }}
         </div>
